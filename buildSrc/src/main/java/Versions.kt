8c40a06d935cc3dae4ddfe1f2d71260a31d48e44
FY <tw-frey@users.noreply.github.com>
2021-09-16 07:38:13 +0800

    Android Gradle Plugin update to 4.2.2
    Signed-off-by: FY <tw-frey@users.noreply.github.com>

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,10 +1,10 @@
 object Versions {
 
     const val androidAppCompat = "1.0.2"
-    const val androidGradlePlugin = "3.5.3"
+    const val androidGradlePlugin = "4.2.2"
     const val androidKtxCore = "1.0.2"
-    const val kotlin = "1.3.61"
-    const val lint = "26.5.3"
+    const val kotlin = "1.5.30"
+    const val lint = "27.2.2"
     const val junitJupiter = "5.5.0"
     const val junitVintage = "5.5.0"
     const val jUnit = "4.12"
